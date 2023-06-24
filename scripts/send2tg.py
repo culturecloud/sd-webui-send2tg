5696d7985383e5b8d36f52028762fedbe3dd1cc9
pseudokawaii@gmail.com <pseudokawaii@gmail.com>
2023-06-24 14:17:51 +0600

    Initial code

---
+++ scripts/send2tg.py
@@ -0,0 +1,87 @@
+import modules.scripts as scripts
+import gradio as gr
+import requests
+import os
+
+from modules import images, shared, script_callbacks
+from modules.processing import process_images
+
+class Script(scripts.Script):
+
+    def title(self):
+        return "Send to Telegram"
+
+    def show(self, _):
+        return scripts.AlwaysVisible
+    
+    def ui(self, _):
+        enable = gr.Checkbox(
+            False,
+            label="Enable"
+        )
+        return [enable]
+    
+    def run(self, p, enable):
+        proc = process_images(p)
+        send_document = f"https://api.telegram.org/bot{shared.opts.send2tg.tg_token}/sendDocument"
+        
+        if enable:
+            for i in range(len(proc.images)):
+                image, txt = images.save_image(
+                    image=proc.images[i],
+                    path=p.outpath_samples,
+                    basename="",
+                    seed=proc.seed,
+                    prompt=proc.prompt,
+                    extension=opts.samples_format,
+                    info=proc.info,
+                    p=p
+                )
+            
+                data = {
+                    "chat_id": shared.opts.send2tg.channel_id,
+                    "parse_mode": "MARKDOWN",
+                    "caption": f"`{proc.prompt}`"
+                }
+            
+                files = {
+                    "document": open(image, "rb")
+                }
+            
+                requests.post(
+                    send_document,
+                    data=data,
+                    files=files,
+                    stream=True
+                )
+            
+        return proc
+
+
+def on_ui_settings():
+    section = ('send2tg', "Send to Telegram")
+    
+    shared.opts.add_option(
+        "bot_token",
+        shared.OptionInfo(
+            ""
+            "Telegram Bot Token",
+            gr.Textbox(
+                info="This bot should have message permission on your channel."
+            ),
+            section=section
+        )
+    )
+    shared.opts.add_option(
+        "channel_id",
+        shared.OptionInfo(
+            "",
+            "Channel ID",
+            gr.Number(
+                info="Channel ID of your Telegram channel where the bot would send the images."
+            ),
+            section=section
+        )
+    )
+        
+script_callbacks.on_ui_settings(on_ui_settings)
