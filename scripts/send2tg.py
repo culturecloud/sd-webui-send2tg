3c389c80f2eeb8c9cd836d5afb8f473d826bc1e4
RedoX <pseudokawaii@gmail.com>
2023-06-28 03:10:26 +0600

    Make it properly work finally!

--- scripts/send2tg.py
+++ scripts/send2tg.py
@@ -1,68 +1,45 @@
 import gradio as gr
 import requests
-import os
 
-from modules import images, scripts, script_callbacks
-from modules.processing import process_images
+from modules import script_callbacks
 from modules.shared import opts, OptionInfo
 
-class Script(scripts.Script):
 
-    def title(self):
-        return "Send to Telegram"
-
-    def show(self, _):
-        return scripts.AlwaysVisible
+def on_image_saved(params):
+    method = "sendDocument" if opts.send2tg_as_document else "sendPhoto"
+    tg_api = f"https://api.telegram.org/bot{opts.send2tg_bot_token}/{method}"
     
-    def ui(self, _):
-        enabled = gr.Checkbox(
-            False,
-            label="Send images to Telegram"
+    if (opts.send2tg_enabled and opts.send2tg_bot_token and opts.send2tg_channel_id):
+        data = {
+            "chat_id": opts.send2tg_channel_id,
+            "parse_mode": "MARKDOWN",
+            "caption": f"`{params.p.prompt}`"
+        }
+                    
+        files = {
+            f"{'document' if method == 'sendDocument' else 'photo'}": open(params.filename, "rb")
+        }
+                    
+        requests.post(
+            tg_api,
+            data=data,
+            files=files,
+            stream=True
         )
-        
-        return [enabled]
+            
     
-    def postprocess(self, p, proc, enabled):
-        
-        method = "sendDocument" if opts.send2tg_as_document else "sendPhoto"
-        tg_api = f"https://api.telegram.org/bot{opts.send2tg_bot_token}/{method}"
-        
-        if (enabled and opts.send2tg_bot_token and opts.send2tg_channel_id):
-            for i in range(len(proc.images)):
-                image, txt = images.save_image(
-                    image=proc.images[i],
-                    path=p.outpath_samples,
-                    basename="",
-                    seed=proc.seed,
-                    prompt=proc.prompt,
-                    extension=opts.samples_format,
-                    info=proc.info,
-                    p=p
-                )
-            
-                data = {
-                    "chat_id": opts.send2tg_channel_id,
-                    "parse_mode": "MARKDOWN",
-                    "caption": f"`{proc.prompt}`"
-                }
-            
-                files = {
-                    f"{'document' if method == 'sendDocument' else 'photo'}": open(image, "rb")
-                }
-            
-                requests.post(
-                    tg_api,
-                    data=data,
-                    files=files,
-                    stream=True
-                )
-            
-        return proc
-        
-
 def on_ui_settings():
     section = ('send2tg', "Send to Telegram")
     
+    opts.add_option(
+        "send2tg_enabled",
+        OptionInfo(
+            default=False,
+            label="Enable Send to Telegram",
+            component=gr.Checkbox,
+            section=section
+        )
+    )
     opts.add_option(
         "send2tg_bot_token",
         OptionInfo(
@@ -73,7 +50,7 @@ def on_ui_settings():
                 "placeholder": "123456789:xxxxxxxxxxxxxxxxxxxxxxxxx"
             },
             section=section,
-            comment_after="(This bot should have message permission to the channel you specify below.)"
+            comment_after="<i>(This bot should have message permission to the channel you specify below.)</i>"
         )
     )
     opts.add_option(
@@ -86,7 +63,7 @@ def on_ui_settings():
                 "placeholder": "-1001234567890"
             },
             section=section,
-            comment_after="(This is the channel where the bot will send images.)"
+            comment_after="<i>(This is the channel where the bot will send images.)</i>"
         )
     )
     opts.add_option(
@@ -95,10 +72,11 @@ def on_ui_settings():
             default=True,
             label="Send as Document",
             component=gr.Checkbox,
-            section=section
+            section=section,
+            comment_after="(Image will be sent as document without compression.)"
         )
     )
 
 
-#script_callbacks.on_image_saved(on_image_saved)
+script_callbacks.on_image_saved(on_image_saved)
 script_callbacks.on_ui_settings(on_ui_settings)
